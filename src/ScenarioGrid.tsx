0a836d802fde5882c7555003788b290105e691c3
pikdonker <pikdonker@hotmail.com>
2025-04-13 18:09:16 +0200

    remove text-decoration

--- src/ScenarioGrid.tsx
+++ src/ScenarioGrid.tsx
@@ -8,7 +8,7 @@ const ScenarioGrid = () => {
       {
         scenarios.sort((a, b) => a.id < b.id ? -1 : 1).map(s =>
           <Grid key={s.id} item container direction="row" justifyContent="center" alignItems="center" xs={2} sx={{ margin: '40px', cursor: 'pointer' }}>
-            <Link to={import.meta.env.VITE_BASEURL + 'scenarios/' + s.id}>
+            <Link to={import.meta.env.VITE_BASEURL + 'scenarios/' + s.id} style={{ textDecoration: "none" }}>
               <Box sx={{
                 border: '1px solid black',
                 borderRadius: '25px',
